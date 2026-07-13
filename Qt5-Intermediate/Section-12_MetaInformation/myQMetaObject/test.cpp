9d9fc73bd6f385062be87b5b965d01c5d0d3b321
Suman Mondal <suman.mandal1020@gmail.com>
2026-07-13 15:34:33 +0530

    add QMetaObject

---
+++ Qt5-Intermediate/Section-12_MetaInformation/myQMetaObject/test.cpp
@@ -0,0 +1,20 @@
+#include "test.h"
+
+test::test(QObject *parent)
+    : QObject{parent}
+{}
+
+void test::doStuff()
+{
+
+}
+
+void test::doStuff(QString param)
+{
+
+}
+
+void test::mySlot()
+{
+
+}
