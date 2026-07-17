5cd6469399f98244ba7dd498f71dcc0bdf46ac78
Suman Mondal <suman.mandal1020@gmail.com>
2026-07-17 18:23:24 +0530

    add basic testing

---
+++ Qt5-Advanced/Section-5_QtTesting/basic_tests/main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QTest>
+#include "dog.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Dog dog;
+    QTest::qExec(&dog);
+
+
+    return QCoreApplication::exec();
+}
